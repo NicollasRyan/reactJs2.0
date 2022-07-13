fc4ab15fda5b22467bf2611517123044d006bb52
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-07-13 16:49:54 -0300

    Components with TypeScript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,15 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.css';
 
+
+interface Repository {
+    name: String;
+    description: String;
+    html_url: String;
+}
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
